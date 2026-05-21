ad537e6cda048328d00a15cfd7074b77cacf26e0
3Matvey <andrianov8m@gmail.com>
2026-05-21 18:01:18 +0300

    fix: execute inventory sequence increment as scalar command

--- Inventra.Infrastructure.Data/Repositories/InventorySequenceProvider.cs
+++ Inventra.Infrastructure.Data/Repositories/InventorySequenceProvider.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using Microsoft.EntityFrameworkCore.Storage;
+
 namespace Inventra.Infrastructure.Data.Repositories;
 
 internal class InventorySequenceProvider(AppDbContext dbContext) : IInventorySequenceProvider
@@ -6,15 +9,61 @@ internal class InventorySequenceProvider(AppDbContext dbContext) : IInventorySeq
         Guid inventoryId,
         CancellationToken cancellationToken = default)
     {
-        var query = dbContext.Database.SqlQuery<long>(
-            $"""
-             INSERT INTO inventory_sequences (inventory_id, next_value)
-             VALUES ({inventoryId}, 2)
-             ON CONFLICT (inventory_id)
-             DO UPDATE SET next_value = inventory_sequences.next_value + 1
-             RETURNING inventory_sequences.next_value - 1 AS "Value"
-             """);
+        var shouldClose = dbContext.Database.GetDbConnection().State != ConnectionState.Open;
 
-        return await query.SingleAsync(cancellationToken);
+        if (shouldClose)
+            await dbContext.Database.OpenConnectionAsync(cancellationToken);
+
+        try
+        {
+            return await ExecuteNextSequenceCommandAsync(inventoryId, cancellationToken);
+        }
+        finally
+        {
+            if (shouldClose)
+                await dbContext.Database.CloseConnectionAsync();
+        }
+    }
+
+    private async Task<long> ExecuteNextSequenceCommandAsync(
+        Guid inventoryId,
+        CancellationToken cancellationToken)
+    {
+        await using var command = CreateCommand(inventoryId);
+        var value = await command.ExecuteScalarAsync(cancellationToken);
+
+        return Convert.ToInt64(value);
+    }
+
+    private System.Data.Common.DbCommand CreateCommand(Guid inventoryId)
+    {
+        var command = dbContext.Database.GetDbConnection().CreateCommand();
+        command.Transaction = dbContext.Database.CurrentTransaction?.GetDbTransaction();
+        command.CommandText = NextSequenceSql();
+        command.Parameters.Add(CreateInventoryIdParameter(command, inventoryId));
+
+        return command;
+    }
+
+    private static System.Data.Common.DbParameter CreateInventoryIdParameter(
+        System.Data.Common.DbCommand command,
+        Guid inventoryId)
+    {
+        var parameter = command.CreateParameter();
+        parameter.ParameterName = "inventoryId";
+        parameter.Value = inventoryId;
+
+        return parameter;
+    }
+
+    private static string NextSequenceSql()
+    {
+        return """
+               INSERT INTO inventory_sequences (inventory_id, next_value)
+               VALUES (@inventoryId, 2)
+               ON CONFLICT (inventory_id)
+               DO UPDATE SET next_value = inventory_sequences.next_value + 1
+               RETURNING inventory_sequences.next_value - 1
+               """;
     }
 }
